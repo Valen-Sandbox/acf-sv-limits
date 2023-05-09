4f7c7f0a083b80c8fa8b36fbbd1027d37132654c
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2023-05-09 09:29:22 -0400

    A little bit of cleanup

--- lua/autorun/server/sv_caliber_limit.lua
+++ lua/autorun/server/sv_caliber_limit.lua
@@ -8,64 +8,65 @@ local cvarFlags = { FCVAR_ARCHIVE, FCVAR_REPLICATED }
 local caliberCvar = CreateConVar( "acf_limits_caliber", 5000, cvarFlags, "The maximum total ACF caliber that a player can have out at once (in mm).", 0, 5000 )
 local overCaliberEnts = {}
 
-hook_Add( "PlayerInitialSpawn", "ACF_Limits_Caliber", function( ply )
-    ply.CaliberTotal = 0
-end )
-
-hook_Add( "ACF_CanCreateEntity", "ACF_Limits_Caliber", function( _, ply, _, _, data )
-    local caliberLimit = caliberCvar:GetInt()
-    local entCaliber = data.Caliber
-
+local function totalClamp( total )
     -- This value should never go below 0
-    if ply.CaliberTotal < 0 then
-        ply.CaliberTotal = 0
-    end
+    if total >= 0 then return end
+    total = 0
+end
 
-    local newTotal = ply.CaliberTotal + entCaliber
-    if newTotal > caliberLimit then return false, "ACF total caliber limit would be reached! (" .. caliberLimit .. " mm)" end
-end )
-
-hook_Add( "ACF_CanUpdateEntity", "ACF_Limits_Caliber", function( ent, data )
-    if ent:GetClass() ~= ( "acf_gun" or "acf_rack" ) then return end
-    local ply = ent:CPPIGetOwner()
-    local caliberLimit = caliberCvar:GetInt()
-    local caliberOld = ent.Caliber
-    local caliberNew = data.Caliber
-
-    -- This value should never go below 0
-    if ply.CaliberTotal < 0 then
-        ply.CaliberTotal = 0
-    end
-
-    local newTotal = ply.CaliberTotal + caliberNew - caliberOld
-    if newTotal > caliberLimit then return false, "ACF total caliber limit would be reached! (" .. caliberLimit .. " mm)" end
-    ply.CaliberTotal = ply.CaliberTotal + caliberNew - caliberOld
-
+local function clearOnRemove( ent, ply, caliber )
     ent:CallOnRemove( "ACF_Limits_Caliber", function()
         if overCaliberEnts[ent] then
             overCaliberEnts[ent] = nil
         end
 
-        if ply.CaliberTotal < 0 then
-            ply.CaliberTotal = 0
-        end
-
-        ply.CaliberTotal = ply.CaliberTotal - caliberNew
+        totalClamp( ply.CaliberTotal )
+        ply.CaliberTotal = ply.CaliberTotal - caliber
     end )
+end
+
+hook_Add( "PlayerInitialSpawn", "ACF_Limits_Caliber", function( ply )
+    ply.CaliberTotal = 0
+end )
+
+hook_Add( "ACF_CanCreateEntity", "ACF_Limits_Caliber", function( class, ply, _, _, data )
+    if class ~= ( "acf_gun" or "acf_rack" ) then return end
+
+    local caliberLimit = caliberCvar:GetInt()
+    local entCaliber = data.Caliber
+
+    totalClamp( ply.CaliberTotal )
+    local newTotal = ply.CaliberTotal + entCaliber
+
+    if newTotal > caliberLimit then return false, "Total caliber limit would be reached! (" .. caliberLimit .. " mm)" end
+end )
+
+hook_Add( "ACF_CanUpdateEntity", "ACF_Limits_Caliber", function( ent, data )
+    if ent:GetClass() ~= ( "acf_gun" or "acf_rack" ) then return end
+
+    local ply = ent:CPPIGetOwner()
+    local caliberLimit = caliberCvar:GetInt()
+    local caliberOld = ent.Caliber
+    local caliberNew = data.Caliber
+
+    totalClamp( ply.CaliberTotal )
+    local newTotal = ply.CaliberTotal + caliberNew - caliberOld
+
+    if newTotal > caliberLimit then return false, "Total caliber limit would be reached! (" .. caliberLimit .. " mm)" end
+
+    ply.CaliberTotal = newTotal
+    clearOnRemove( ent, ply, caliberNew )
 end )
 
 hook_Add( "OnEntityCreated", "ACF_Limits_Caliber", function( ent )
     timer_Simple( 0, function()
         if not IsValid( ent ) or ent:GetClass() ~= ( "acf_gun" or "acf_rack" ) then return end
+
         local ply = ent:CPPIGetOwner()
         local caliberLimit = caliberCvar:GetInt()
         local entCaliber = ent.Caliber
 
-        -- This value should never go below 0
-        if ply.CaliberTotal < 0 then
-            ply.CaliberTotal = 0
-        end
-
+        totalClamp( ply.CaliberTotal )
         local newTotal = ply.CaliberTotal + entCaliber
 
         if newTotal > caliberLimit then
@@ -73,18 +74,7 @@ hook_Add( "OnEntityCreated", "ACF_Limits_Caliber", function( ent )
         end
 
         ply.CaliberTotal = ply.CaliberTotal + entCaliber
-
-        ent:CallOnRemove( "ACF_Limits_Caliber", function()
-            if overCaliberEnts[ent] then
-                overCaliberEnts[ent] = nil
-            end
-
-            if ply.CaliberTotal < 0 then
-                ply.CaliberTotal = 0
-            end
-
-            ply.CaliberTotal = ply.CaliberTotal - ent.Caliber
-        end )
+        clearOnRemove( ent, ply, ent.Caliber )
     end )
 end )
 
