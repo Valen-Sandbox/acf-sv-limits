46231e76e7f069c9dc253e913bad113fa1ca52cd
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2023-07-05 12:54:59 -0400

    Disable overweight engines

--- lua/autorun/server/sv_mass_limit.lua
+++ lua/autorun/server/sv_mass_limit.lua
@@ -1,10 +1,16 @@
 local cvarFlags = { FCVAR_ARCHIVE, FCVAR_REPLICATED }
 local massCvar = CreateConVar( "acf_limits_mass", 61000, cvarFlags, "The maximum mass that a contraption may have before its weapons are disabled (in whole kg). Set to 0 to disable this limit.", 0 )
 
+local disabledEnts = {
+    ["acf_gun"] = true,
+    ["acf_rack"] = true,
+    ["acf_engine"] = true
+}
+
 hook.Add( "ACF_IsLegal", "ACF_Limits_Mass", function( ent )
     local massLimit = massCvar:GetInt()
     if massLimit == 0 then return end
-    if ent:GetClass() ~= ( "acf_gun" or "acf_rack" ) then return end
+    if not disabledEnts[ent:GetClass()] then return end
 
     local con = ent:GetContraption()
     if not con then return end
