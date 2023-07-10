30758345f614af8e18b623c41200ad3a00b96698
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2023-07-10 11:26:58 -0400

    Don't disable munitions under limits

--- lua/autorun/server/sv_missile_limit.lua
+++ lua/autorun/server/sv_missile_limit.lua
@@ -1,8 +1,10 @@
 local IsValid = IsValid
 
 local cvarFlags = { FCVAR_ARCHIVE, FCVAR_REPLICATED }
-local missileCvar = CreateConVar( "acf_limits_missiles", 8, cvarFlags, "The maximum number of missiles that a contraption may hold before its racks are disabled. Set to 0 to disable this limit.", 0 )
-local rocketCvar = CreateConVar( "acf_limits_rockets", 18, cvarFlags, "The maximum number of rockets that a contraption may hold before its racks are disabled. Set to 0 to disable this limit.", 0 )
+local missileCvar = CreateConVar( "acf_limits_missiles", 8, cvarFlags, "The maximum number of missiles that a contraption may hold before further racks are disabled. Set to 0 to disable this limit.", 0 )
+local rocketCvar = CreateConVar( "acf_limits_rockets", 18, cvarFlags, "The maximum number of rockets that a contraption may hold before further racks are disabled. Set to 0 to disable this limit.", 0 )
+
+local overLimitEnts = {}
 
 hook.Add( "cfw.contraption.created", "ACF_Limits_Missiles", function( con )
     if not con then return end
@@ -16,13 +18,21 @@ hook.Add( "cfw.contraption.entityAdded", "ACF_Limits_Missiles", function( con, e
     if ent:GetClass() ~= "acf_rack" then return end
 
     if ent.EntType == "Pod" then
-        if rocketCvar:GetInt() == 0 then return end
+        local rocketLimit = rocketCvar:GetInt()
+        if rocketLimit == 0 then return end
 
         con.totalRockets = con.totalRockets + ent.MagSize
+        if con.totalRockets > rocketLimit then
+            overLimitEnts[ent] = true
+        end
     else
-        if missileCvar:GetInt() == 0 then return end
+        local missileLimit = missileCvar:GetInt()
+        if missileLimit == 0 then return end
 
         con.totalMissiles = con.totalMissiles + ent.MagSize
+        if con.totalMissiles > missileLimit then
+            overLimitEnts[ent] = true
+        end
     end
 end )
 
@@ -39,9 +49,14 @@ hook.Add( "cfw.contraption.entityRemoved", "ACF_Limits_Missiles", function( con,
 
         con.totalMissiles = con.totalMissiles - ent.MagSize
     end
+
+    if overLimitEnts[ent] then
+        overLimitEnts[ent] = nil
+    end
 end )
 
 hook.Add( "ACF_IsLegal", "ACF_Limits_Missiles", function( ent )
+    if not overLimitEnts[ent] then return end
     if ent:GetClass() ~= "acf_rack" then return end
 
     local con = ent:GetContraption()
@@ -51,11 +66,19 @@ hook.Add( "ACF_IsLegal", "ACF_Limits_Missiles", function( ent )
         local rocketLimit = rocketCvar:GetInt()
         if rocketLimit == 0 then return end
 
-        if con.totalRockets > rocketLimit then return false, "Rocket Count Limited", "Your contraption would surpass the rocket limit (" .. rocketLimit .. " rockets) and has been disabled." end
+        if con.totalRockets > rocketLimit then
+            return false, "Rocket Count Limited", "Your weapon would surpass the rocket limit (" .. rocketLimit .. " rockets) and has been disabled."
+        else
+            overLimitEnts[ent] = nil
+        end
     else
         local missileLimit = missileCvar:GetInt()
         if missileLimit == 0 then return end
 
-        if con.totalMissiles > missileLimit then return false, "Missile Count Limited", "Your contraption would surpass the missile limit (" .. missileLimit .. " missiles) and has been disabled." end
+        if con.totalMissiles > missileLimit then
+            return false, "Missile Count Limited", "Your weapon would surpass the missile limit (" .. missileLimit .. " missiles) and has been disabled."
+        else
+            overLimitEnts[ent] = nil
+        end
     end
 end )
