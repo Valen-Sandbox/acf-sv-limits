c43506482590f1313170fd1cca67052279b1f38e
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2023-07-25 15:34:13 -0400

    Add parent distance limit

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Please be aware that this addon is still fairly WIP and subject to change in beh
 - **acf_limits_caliber**: The maximum total ACF caliber that a player can have out at once (in mm). Set to 0 to disable limitations.
 - **acf_limits_mass**: The maximum mass that a contraption may have before its weapons/engines are disabled (in whole kg). Set to 0 to disable limitations.
 - **acf_limits_missiles**: The maximum number of missiles that a contraption may hold before further racks are disabled. Set to 0 to disable limitations.
+- **acf_limits_parentdist**: The maximum distance that parented weapons/engines/ammo boxes can be from their root parent (in Source units). Set to 0 to disable limits.
 - **acf_limits_rockets**: The maximum number of rockets that a contraption may hold before further racks are disabled. Set to 0 to disable limitations.
 - **acf_limits_worldweld**: Removes the ability to weld contraptions to the world if enabled. This is intended to prevent certain exploits that use this method.
 
---
+++ lua/acf_limits/parentdist_limit_sv.lua
@@ -0,0 +1,29 @@
+local cvarFlags = { FCVAR_ARCHIVE, FCVAR_REPLICATED }
+local distCvar = CreateConVar( "acf_limits_parentdist", 512, cvarFlags, "The maximum distance that parented weapons/engines/ammo boxes can be from their root parent (in Source units). Set to 0 to disable limits.", 0 )
+
+local disabledEnts = {
+    ["acf_gun"] = true,
+    ["acf_rack"] = true,
+    ["acf_engine"] = true,
+    ["acf_radar"] = true,
+    ["acf_ammo"] = true
+}
+
+hook.Add( "ACF_IsLegal", "ACF_Limits_ParentDist", function( ent )
+    local distLimit = distCvar:GetInt()
+    if distLimit == 0 then return end
+    if not disabledEnts[ent:GetClass()] then return end
+
+    local fam = ent:GetFamily()
+    if not fam then return end
+
+    local root = fam:GetRoot()
+    if not root then return end
+
+    local entPos = ent:GetPos()
+    local rootPos = root:GetPos()
+
+    if entPos:DistToSqr( rootPos ) > distLimit * distLimit then
+        return false, "Parent Distance Limited", "Your component would surpass the root parent distance limit (" .. distLimit .. " units) and has been disabled."
+    end
+end )
